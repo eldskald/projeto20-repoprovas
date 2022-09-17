0932be4e14410b61fa6bd2beb6033c5d286d6d8a
rafael <rafaelbordoni00@gmail.com>
2022-09-17 07:53:54 -0300

    Fix wrong endpoint addresses

--- src/routers/authRouter.ts
+++ src/routers/authRouter.ts
@@ -5,6 +5,6 @@ import signInSchema from '../schemas/signInSchema';
 import signUpSchema from '../schemas/signUpSchema';
 
 const authRouter = Router();
-authRouter.post('/signIn', schemaValidation(signInSchema), signIn);
-authRouter.post('/signUp', schemaValidation(signUpSchema), signUp);
+authRouter.post('/sign-in', schemaValidation(signInSchema), signIn);
+authRouter.post('/sign-up', schemaValidation(signUpSchema), signUp);
 export default authRouter;
